883d8cfa8c1848591c516f80f37aff49a36d6080
Aileen <aileenpadrontorres511@gmail.com>
2024-02-11 21:53:38 +0000

    Fixed api by server

--- server/server.js
+++ server/server.js
@@ -1,31 +1,38 @@
-import express from 'express';
-import axios from 'axios';
-import cors from 'cors';
+import express from 'express'
+import axios from 'axios'
+import cors from 'cors'
+import https from 'https'
 
-const app = express();
-const PORT = 3001;
+const app = express()
+const PORT = 3001
 
 // Middleware para permitir solicitudes de origen cruzado (CORS)
-app.use(cors());
-
+app.use(cors())
+const httpsAgent = new https.Agent({ rejectUnauthorized: false })
 app.get('/', (req, res) => {
-    res.send('¡Bienvenido a la API de citas aleatorias!');
-});
+  res.send('¡Bienvenido a la API de citas aleatorias!')
+})
 
 // Ruta para manejar la solicitud a /api/quotes/random
 app.get('/api/quotes/random', async (req, res) => {
   try {
     // Hacer la solicitud a la API quotable.io
-    const response = await axios.get('https://api.quotable.io/quotes/random');
+    console.log('Haciendo solicitud a la API quotable.io')
+    const response = await axios.get('https://api.quotable.io/quotes/random', {
+      httpsAgent,
+    })
     // Devolver los datos recibidos desde la API
-    res.status(200).json(response.data);
+    res.status(200).json(response.data[0])
   } catch (error) {
     // Manejar cualquier error que ocurra
-    res.status(500).json({ error: 'Ocurrió un error al obtener la cita aleatoria' });
+    console.error('Petó')
+    res.status(500).json({
+      error: `Ocurrió un error al obtener la cita aleatoria: ${error.message}`,
+    })
   }
-});
+})
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor Express corriendo en el puerto ${PORT}`);
-});
+  console.log(`Servidor Express corriendo en el puerto ${PORT}`)
+})
